cd2d4d223fcc8ca43970b506b472d283647787a2
pasindurathnayake <pshalinda5@gmail.com>
2026-04-09 19:26:14 +0530

    Setting up tailwindcss and shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,22 @@
-import { useEffect, useState } from "react"
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import { Button } from '@/components/ui/button.tsx';
 
 function App() {
-  const [message, setMessage] = useState('');
+   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch('/api/hello')
-      .then(res => res.json())
-      .then(data => setMessage(data.message));
-  }, []);
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((res) => res.json())
+         .then((data) => setMessage(data.message));
+   }, []);
 
-  return <p>{message}</p>
+   return (
+      <div className="p-4">
+         <p className="font-bold text-3xl">{message}</p>
+         <Button>Click me</Button>
+      </div>
+   );
 }
 
-export default App
+export default App;
